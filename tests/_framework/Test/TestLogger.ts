0dd7e9ff1fb1a0f2e9901406caaf4082d17fecbf
EGRrqq <egrrqqdev@gmail.com>
2024-11-04 12:37:33 -0500

    test: log error data

--- tests/_framework/Test/TestLogger.ts
+++ tests/_framework/Test/TestLogger.ts
@@ -4,20 +4,22 @@ import type { ITestEntity } from "../model/ITestEntity";
 const whiteSpace = "   ";
 const separator = "----------------------------------";
 const greenText = (text: string) => `\x1b[42m${text}\x1b[0m`;
+const redText = (text: string) => `\x1b[41m${text}\x1b[0m`;
 const boldText = (text: string) => `\x1b[1m${text}\x1b[0m`;
 
 export class TestLogger {
   // Log the test results based on the success flag
   static log = async <T>(testEntity: ITestEntity<T>) =>
-    this.#successFlag(testEntity)
-      ? this.#successLog<T>(testEntity)
-      : this.#errorLog();
+    (await this.#successFlag(testEntity))
+      ? await this.#successLog<T>(testEntity)
+      : await this.#errorLog(testEntity);
 
   // Determine if the test was successful
-  static #successFlag = <T>(e: ITestEntity<T>) => e.received === e.expected;
+  static #successFlag = async <T>(e: ITestEntity<T>) =>
+    e.received === e.expected;
 
   // Log the success message
-  static #successLog = <T>(e: ITestEntity<T>) => {
+  static #successLog = async <T>(e: ITestEntity<T>) => {
     console.log(
       "\n",
       `${greenText("  ")}`,
@@ -31,8 +33,23 @@ export class TestLogger {
   };
 
   // Log the error message
-  static #errorLog = () => {
-    throw Error("pants down");
+  static #errorLog = async <T>(e: ITestEntity<T>) => {
+    console.log(
+      "\n",
+      `${redText("  ")}`,
+      `${boldText("name")}: ${e.name}`,
+      "\n",
+      `${whiteSpace}${boldText("path")}: ${__filename}`,
+      "\n",
+      `${whiteSpace}${boldText("time")}: ${e.duration.toFixed(4)} ms`,
+      "\n",
+      `${whiteSpace}${boldText("expected")}: ${e.expected}`,
+      "\n",
+      `${whiteSpace}${boldText("received")}: ${e.received}`,
+      `\n\n ${separator}`
+    );
+
+    throw Error(`${e.expected} not equal to ${e.received}`);
   };
 }
 
