c3d931da15246f3194ac1c973b209b614a60d4ef
EGRrqq <egrrqqdev@gmail.com>
2024-11-04 08:18:15 -0500

    test: isProp change ! check to undefined

--- tests/_framework/utils/IsProp.ts
+++ tests/_framework/utils/IsProp.ts
@@ -2,7 +2,7 @@ type TIsProp = <T>(prop: T | undefined, propName: string) => asserts prop is T;
 
 // Ensure a specific property is assigned
 export const isProp: TIsProp = (prop, propName) => {
-  if (!prop)
+  if (prop === undefined)
     throw Error(`Property '${propName}' is not assigned to the entity`);
 };
 
